8eaf646b3a9e52318826d66206c20735b5e40760
8bitaby <abyvjab@gmail.com>
2023-03-17 10:24:01 +0530

    First openAI testing

--- initial.py
+++ initial.py
@@ -1,1 +1,10 @@
-print("Hello world")
+import openai
+import os
+from dotenv import load_dotenv  # python-dotenv method
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+response = openai.Completion.create(
+    model='text-davinci-003',
+    prompt='Give me two reasons to learn OpenAI API with python',
+    max_tokens=300)
+print(response)
